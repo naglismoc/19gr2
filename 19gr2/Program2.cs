8b5d805a1f7269d6328c58ec00d18020d47ade3c
naglis mockevicius <48673743+naglismoc@users.noreply.github.com>
2021-11-25 21:06:14 +0200

    OOP

---
+++ 19gr2/Program2.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _19gr2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Student studentas0 = new Student( 30, "Kaunas");
+
+            Student studentas = new Student();
+            studentas.Name = "Petras";
+            studentas.Surname = "Adomaitis";
+            studentas.Age = 19;
+
+            //Console.WriteLine(studentas);
+
+            Student studentas2 = new Student();
+            studentas2.Name = "Petras2";
+            studentas2.Surname = "Adomaitis2";
+            studentas2.Age = 192;
+
+            //Console.WriteLine(studentas2.ToString());
+
+            List<Student> students = new List<Student>() { studentas, studentas0, studentas2 };
+
+            foreach (var student in students)
+            {
+                Console.WriteLine(student);
+            }
+
+        }
+
+        public static void sayHi()
+        {
+            Console.WriteLine("helleu");
+        }
+
+        public static string returnHi()
+        {
+            return "Hi";
+        }
+
+
+        public static void sayHiP(string name)
+        {
+            Console.WriteLine("helleu "+ name);
+        }
+
+        public static string returnHiP(string name)
+        {
+            return "Hi " +name;
+        }
+    }
+}
---
+++ 19gr2/Student.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19gr2
+{
+    class Student
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public string address { get; set; }
+
+        public int Grade { get; set; }
+        public Student()
+        {
+        }
+        public Student(  int age, string address, string surname = "doe", string name = "john")
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+            this.address = address;
+            this.Grade = 1;
+        }
+
+          public Student(string name, string surname,  string address, int age)
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+            this.address = address;
+        }
+
+        public override string ToString()
+        {
+            return "Studento vardas yra "+Name+", pavarde "+Surname+", amzius "+Age;
+        }
+
+        public static void sayBie()
+        {
+            Console.WriteLine("bubbie");
+        }
+    }
+}
