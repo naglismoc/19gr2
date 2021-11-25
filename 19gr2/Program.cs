df8c5fe3ee21942cbad01eb8bdf1a1c91214335f
nagli <nagli@DESKTOP-4O6V3JC>
2021-11-25 18:21:37 +0200

    Add project files.

---
+++ 19gr2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _19gr2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string>();
+
+            names.Add("Ramunas");
+            names.Add("Vaidas");
+            names.Add("Tautvydas");
+            names.Add("Vilma");
+
+
+            List<string> first = new List<string> { "One", "Two", "Three" };
+            List<string> second = new List<string>() { "Four", "Five" };
+        }
+    }
+}
