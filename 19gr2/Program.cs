a032230f653ed5d31a045e98a3e6b892979d7c1e
naglis mockevicius <48673743+naglismoc@users.noreply.github.com>
2021-11-25 19:02:53 +0200

    Update Program.cs

--- 19gr2/Program.cs
+++ 19gr2/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace _19gr2
@@ -7,16 +7,36 @@ namespace _19gr2
     {
         static void Main(string[] args)
         {
-            List<string> names = new List<string>();
+            //List<string> names = new List<string>();
 
-            names.Add("Ramunas");
-            names.Add("Vaidas");
-            names.Add("Tautvydas");
-            names.Add("Vilma");
+            //names.Add("Ramunas");
+            //names.Add("Vaidas");
+            //names.Add("Tautvydas");
+            //names.Add("Vilma");
 
 
-            List<string> first = new List<string> { "One", "Two", "Three" };
-            List<string> second = new List<string>() { "Four", "Five" };
+            //List<string> first = new List<string> { "One", "Two", "Three" };
+            //List<string> second = new List<string>() { "Four", "Five" };
+
+            //names.AddRange(first);
+
+            //foreach (string vardas in names)
+            //{
+            //    Console.WriteLine(vardas);
+            //}
+            //Console.WriteLine(names.Count);
+            //names.Remove("Vaidas");
+            //names.RemoveAt(3);
+            //names.ForEach(vardas => { Console.WriteLine(vardas); });
+
+            Random rnd = new Random();
+            List<int> nums = new List<int>();
+            for (int i = 0; i < 30; i++)
+            {
+                nums.Add(rnd.Next(5, 26));
+            }
+            nums.ForEach(nr => { Console.WriteLine(nr); });
+
         }
     }
 }
