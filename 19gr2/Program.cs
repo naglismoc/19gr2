a97604f8d37a8638fe8870ec6cf774a6ce267b71
naglis mockevicius <48673743+naglismoc@users.noreply.github.com>
2021-11-25 20:11:41 +0200

    Update Program.cs

--- 19gr2/Program.cs
+++ 19gr2/Program.cs
@@ -29,14 +29,47 @@ namespace _19gr2
             //names.RemoveAt(3);
             //names.ForEach(vardas => { Console.WriteLine(vardas); });
 
-            Random rnd = new Random();
-            List<int> nums = new List<int>();
-            for (int i = 0; i < 30; i++)
-            {
-                nums.Add(rnd.Next(5, 26));
-            }
-            nums.ForEach(nr => { Console.WriteLine(nr); });
+            //Random rnd = new Random();
+            //List<int> nums = new List<int>();
+            //for (int i = 0; i < 30; i++)
+            //{
+            //    nums.Add(rnd.Next(5, 26));
+            //}
+            //nums.ForEach(nr => { Console.WriteLine(nr); });
+
+
+            //a. Suskaičiuokite kiek masyve yra reikšmių didesnių už 10;
+            //int counter = 0;
+            //for (int i = 0; i < nums.Count; i++)
+            //{
+            //    if(nums[i] > 10)
+            //    {
+            //        counter++;
+            //    }
+            //}
+            //Console.WriteLine(counter);
+            ////b. Raskite didžiausią masyvo reikšmę ir jos indeksą arba indeksus jeigu yra keli ir atspausdinkite.
+
+            //int maxVal = 0;
+
+            //List<int> indexes = new List<int>();
+            //for (int i = 0; i < nums.Count; i++)
+            //{
+            //    if(nums[i] > maxVal)//25
+            //    {
+            //        maxVal = nums[i];
+            //        indexes.Clear();
+            //        indexes.Add(i);
+            //    }
+            //    if(nums[i] == maxVal)
+            //    {
+            //        indexes.Add(i);
+            //    }
+            //}
+
+
 
         }
+
     }
 }
